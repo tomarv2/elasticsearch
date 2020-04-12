2303ae451bdfb68466c9ddf0f8c5fdaa09c8429f
tomarv2 <varuntomar2019@gmail.com>
2020-04-12 08:45:48 -0700

    initial commit

--- elasticsearch/pipelines/load.sh
+++ elasticsearch/pipelines/load.sh
@@ -5,8 +5,8 @@ do
 	echo "Creating pipeline in file - $f"
 
 FILENAME=$f
-curl -X PUT -H 'Content-Type: application/json' -d @$FILENAME http://es.devlabs.xyz.com/_ingest/pipeline/$FILENAME
+curl -X PUT -H 'Content-Type: application/json' -d @$FILENAME http://es.demo.xyz.com/_ingest/pipeline/$FILENAME
 #curl -X PUT -d @$FILENAME http://es.xyz.com/_ingest/pipeline/$PIPELINE
-#curl -X PUT -d @$FILENAME http://es.devlabs.xyz.com/_ingest/pipeline/$PIPELINE
+#curl -X PUT -d @$FILENAME http://es.demo.xyz.com/_ingest/pipeline/$PIPELINE
 
 done
